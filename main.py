1d87145f3ea08bf661ac4300b96601947608288b
Muhammad Usman Shaukat <32989955+usmanshaukat5@users.noreply.github.com>
2017-10-23 02:17:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from flask import Flask, render_template,json,jsonify
 from flask.ext.mysqldb import MySQL
+from flask_ask import Ask, statement, question
+import logging
+from random import randint
 
 app = Flask(__name__)
+query=""
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'test'
 mysql = MySQL(app)
 
+ask = Ask(app, "/")
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+@ask.intent("DisplayMeIsIntent")
+def display_me(displayme):
+    global query
+    query = displayme
+    return statement("Please refresh browser.")
+
 @app.route('/')
 def index():
 	cur = mysql.connection.cursor()
-	cur.execute('''SELECT * FROM country''')
+	cur.execute(query)
 	wishes = cur.fetchall()
 	wishes_dict = []
 	for wish in wishes:
